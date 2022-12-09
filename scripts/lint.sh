b9f38b0b2a24293f54df2a97aff821dcd0a5b226
0xean <0xean.eth@gmail.com>
2022-12-09 15:14:27 -0700

    remove proto buf check

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,11 +6,6 @@ die() {
 	exit 1
 }
 
-# Check that no .pb.go files were added.
-if git ls-files '*.go' | grep -q '.pb.go$'; then
-	die "Do not add generated protobuf .pb.go files"
-fi
-
 if [ -n "$(git ls-files '*.go' | grep -v -E '.pb.gw.go' | grep -v -e '^docs/' | xargs gofumpt -l 2>/dev/null)" ]; then
 	git ls-files '*.go' | grep -v -E '.pb.gw.go' | grep -v -e '^docs/' | xargs gofumpt -d 2>/dev/null
 	die "Go formatting errors"
